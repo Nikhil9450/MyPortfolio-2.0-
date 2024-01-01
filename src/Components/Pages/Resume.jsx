fc30c5dfd12232fa6d3c9bd6230e45f25102a735
Nikhil Kushwaha <osndl.nikhilk@aicofindia.com>
2024-01-01 14:51:15 +0530

    updated resume

--- src/Components/Pages/Resume.jsx
+++ src/Components/Pages/Resume.jsx
@@ -6,236 +6,76 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 const Resume = () => {
   return (
     <div className='resume_container'>
-      <div class="grid-container">
-        {/* <div class="grid-item">
-        <div>
-          <h3>EDUCATION</h3>
-           <div  className='grid-content'>
-              <div>
-                <h4>MCA-(75%)</h4>
-                <p>Institute of Management Studies,Ghaziabad</p>
-                <p>06/2019-07/2021</p>
-              </div>
-              <div>
-                <h4>BCA-(60%)</h4>
-                <p>School of Management Sciences,Varanasi</p>
-                <p>06/2016-06/2019</p>
-              </div>
-              <div>
-                <h4>Intermediate-(89%)</h4>
-                <p>SHRI P.N.I.C. PARSAHI SAKARA GHAZIPUR</p>
-                <p>03/2015-04/2016</p>
-              </div>
-              <div>
-                <h4>High School-(84%)</h4>
-                <p>B.S. INTER COLLEGE ADARSH GAOV GHAZIPUR</p>
-                <p>04/2013-05/2014</p>
-              </div>
-           </div>
-        </div>       
-        </div>
-
-        <div class="grid-item">
-              <h3>SKILLS</h3>
-              <div  className='grid-content'>
-                <div class="skill-grid-container">
-                  <div class="skill-grid-item"><p>HTML</p></div>
-                  <div class="skill-grid-item"><p>CSS</p></div>
-                  <div class="skill-grid-item"><p>Javascript</p></div>
-                  <div class="skill-grid-item"><p>Bootstrap</p></div>
-                  <div class="skill-grid-item"><p>React.js</p></div>
-                  <div class="skill-grid-item"><p>My Sql</p></div>
-                  <div class="skill-grid-item"><p>Django</p></div>
-                </div>
-          </div>
-        <div>
-      </div>
-
-
-        </div>
-        <div class="grid-item">
-          <h3>PERSONAL PROJECTS</h3>
-             <div  className='grid-content'>
-                <div>
-                  <h4>My portfolio</h4>
-                  <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p>
-                  <a href='https://my-portfolio-a564b.web.app/'>https://my-portfolio-a564b.web.app/ </a>
-                </div>
-                <div>
-                  <h4>Food Order App</h4>
-                  <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p>
-                </div>
-                <div>
-                  <h4> Keeper App</h4>
-                  <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p>
-                </div>
-                <div>
-                  <h4> SpaceX missions</h4>
-                  <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p>
-                  <a href='https://authentication-ae8a1.web.app/'>https://authentication-ae8a1.web.app/</a>
-                </div>
-             </div>
-         </div>
-
-        <div class="grid-item">
-            <h3>CERTIFICATES</h3>
-             <div  className='grid-content'>
-              <ul>
-                <li>Certificate for Completion of CPP Training (03/2018)</li>
-                <li><p>Certificate for Completion of Java Training (10/2018)</p></li>
-                <li><p>Certificate for Participating in Enterpreneurship
-                  Awereness Camp (01/2019)</p></li>
-                <li><p>Certificate for Participating in Workshop on Developing
-                    Standalone Application Using Core Java conducted by
-                    SB Soft Technologies (09/2018)</p></li>
-                
-              </ul>     
-             </div>
-        </div>
-
-
-        <div class="grid-item">
-          <h3>LANGUAGES</h3>
-          <div  className='grid-content'>
-            <div>
-              <h4>English</h4>
-              <p>Full Professional Proficiency</p>
-            </div>
-            <div>
-              <h4>Hindi</h4>
-              <p>Native or Bilingual Proficiency</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="grid-item">
-          <h3>HOBBIES</h3>
-         <div className='grid-content'>
-            <p>Playing PC Games</p>
-         </div>
-        </div> */}
-        <div class="grid-item item1">
+      <div className='row1'>
+        <div className='col1 item'>
           <div className='profilecontainer'>
-            <img class="profile" src={img3} />
-            <h2>Nikhil Kushwaha</h2>
-            <h5>Web Developer</h5>
-            <p>
-            Seeking a responsible job with an opportunity for professional challenges where my ability and professional skills can be fully utilized, and learn new technology and while serving the company with sincerity and dedication.
-            </p>
-          </div>
-          <div className='progressbarContainer'>
-            <label>HTML</label>
-            <ProgressBar variant="success" now={40} />
-            <label>HTML</label>
-            <ProgressBar variant="success" now={20} />
-            <label>HTML</label>
-            <ProgressBar variant="success" now={60} />
-            <label>HTML</label>
-            <ProgressBar variant="success" now={80} />
+              <img class="profile" src={img3} />
+              <h2>Nikhil Kushwaha</h2>
+              <h5>Web Developer</h5>
+              <p>
+              Seeking a responsible job with an opportunity for professional challenges where my ability and professional skills can be fully utilized, and learn new technology and while serving the company with sincerity and dedication.
+              </p>
           </div>
         </div>
-        <div class="grid-item item2">
-          <div class="item2_1 grid2-item">
-            <div>
-            <h4>CERTIFICATES</h4>
-             <div  className='grid-content'>
-              <ul>
-                <li>Certificate for Completion of CPP Training (03/2018)</li>
-                <li><p>Certificate for Completion of Java Training (10/2018)</p></li>
-                <li><p>Certificate for Participating in Enterpreneurship
-                  Awereness Camp (01/2019)</p></li>
-                <li><p>Certificate for Participating in Workshop on Developing
-                    Standalone Application Using Core Java conducted by
-                    SB Soft Technologies (09/2018)</p></li>
-                
-              </ul>     
-             </div>
+        <div className='col2 item'>
+            <h4>EXPERIENCE</h4>
+            <div className='experience'>
+              <p className='duration'>September 2022 - Present</p>
+              <h5>Software Developer</h5>
+              <h5>AGRICULTURE INSURANCE COMPANY OF INDIA LIMITED / India, New Delhi</h5>
+              <p>As a Software Developer at AGRICULTURE INSURANCE COMPANY OF INDIA 
+                  LIMITED, I was responsible for developing and maintaining frontend applications to support the company's 
+                  operations. During my time at the company, I gained valuable experience in 
+                  HTML, CSS, Javascript, Django.</p>
             </div>
-            <div  className='projectsection'>
-              <div>
-                <h4>PROJECTS</h4>
-              </div>
-              <div className='project'>
-                <div>
-                  <h5>My portfolio</h5>
-                </div>
-                <div>
-                <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p>
-                  <a href='https://my-portfolio-a564b.web.app/'>https://my-portfolio-a564b.web.app/ </a>
-                </div>
-                <div>
-                  <h5>Food Order App</h5>
-                </div>
-                <div>
-                <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p>
-                </div>
-                <div>
-                  <h5> Keeper App</h5>
-                </div>
-                <div>
-                <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p>
-                </div>
-                <div>
-                  <h5> SpaceX missions</h5>
-                </div>
-                <div>
-                  <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p>
-                  <a href='https://authentication-ae8a1.web.app/'>https://authentication-ae8a1.web.app/</a>
-                </div>
-               </div>
-                
-             </div>
-          </div> 
-          <div class="item2_2 grid2-item">
-            <div class="item2_2_1 childof2_2">
-              <div>
-                <h4>EDUCATION</h4>
-              </div>
-              <div class="education">
-                 <div>
-                   <p>06/2016-06/2019</p>
-                 </div>
-                 <div>
-                    <h5>Bachelor's in Computer Applications.</h5>
-                    <p>School of Management Sciences,Varanasi</p>
-                 </div>
-                 <div>
-                    <p>06/2019-07/2021</p>
-                 </div>
-                 <div>
-                    <h5>Masters's in Computer Applications.</h5>
-                    <p>Institute of Management Studies,Ghaziabad</p>
-                 </div>
-              </div>
-             
+            <div className='experience'>
+                <p className='duration'>June 2022 - August 2022</p>
+                <h5>Developer Intern</h5>
+                <h5>AGRICULTURE INSURANCE COMPANY OF INDIA LIMITED / India, New Delhi</h5>
+                <p>During my time as a Developer Intern at Ganit Inc. I was able to gain valuable hands-on experience in React js and 
+                    project management. Working alongside a team of experienced developers, 
+                    I had the opportunity to contribute to various projects and learn from their 
+                    expertise. Through this internship, I was able to enhance my technical skills, 
+                    problem-solving abilities, and communication skills.</p>
             </div>
-            {/* <div class="item2_2_2 childof2_2">
-
-            </div>
-            <div class="item2_2_3 childof2_2">
-
-            </div> */}
-            <div class="item2_2_4 childof2_2">
-              <div>
-              <h4>LANGUAGES</h4>
-              </div>
-              <div>
-                <div  className='grid-content'>
-                  <div>
-                    <h5>English</h5>
-                    <p>Full Professional Proficiency</p>
-                  </div>
-                  <div>
-                    <h5>Hindi</h5>
-                    <p>Native or Bilingual Proficiency</p>
-                  </div>
-                </div>
-              </div> 
-            </div>                                    
-          </div> 
         </div>
-   
-     </div>
+
+        <div className='col3 item'>
+          <div  className='projectsection'>
+                <div>
+                  <h4>PERSONAL PROJECTS</h4>
+                </div>
+                <div className='project'>
+                  <div>
+                    <h5>My portfolio</h5>
+                  </div>
+                  <div>
+                  {/* <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p> */}
+                    <a href='https://my-portfolio-a564b.web.app/'>https://my-portfolio-a564b.web.app/ </a>
+                  </div>
+                  <div>
+                    <h5>Food Order App</h5>
+                  </div>
+                  <div>
+                  {/* <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p> */}
+                  </div>
+                  <div>
+                    <h5> Keeper App</h5>
+                  </div>
+                  <div>
+                  {/* <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p> */}
+                  </div>
+                  <div>
+                    <h5> SpaceX missions</h5>
+                  </div>
+                  <div>
+                    {/* <p>Using React, HTML,CSS, Javascript,Bootstrap,Firebase</p> */}
+                    <a href='https://authentication-ae8a1.web.app/'>https://authentication-ae8a1.web.app/</a>
+                  </div>
+                </div>   
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
