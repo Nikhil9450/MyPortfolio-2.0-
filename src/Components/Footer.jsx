c8339752805f80c0eafdb393c7cc776a7c034ee0
Nikhil Kushwaha <osndl.nikhilk@aicofindia.com>
2024-01-05 17:39:06 +0530

    updated footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./Footer.css";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter,faGithub } from '@fortawesome/free-brands-svg-icons';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer_container'>
+       <div className='name_position'>
+          <h1 className='footer_name'>Nikhil Kushwaha</h1>
+          <h1 className='position'>React Developer</h1>
+       </div>
+       <div>
+        <h5>Excellence in Frontend Development.</h5>
+       </div>
+       <div className='footer_socialLinks'>
+          <a href='https://www.linkedin.com/in/nikhil-k-14159810b'><FontAwesomeIcon icon={faLinkedin} size="lg"/></a>
+          <a href='https://twitter.com/Nikhilk9839?t=oe0RHy9pCeCuR6lrHu-1GA&s=08'><FontAwesomeIcon icon={faTwitter} size="lg"/></a>
+          <a href='https://github.com/Nikhil9450'><FontAwesomeIcon icon={faGithub} size="lg"/></a>
+        </div> 
+    </div>
   )
 }
 
